de00b88a511f1840e1270ca7eb313f11ab1134f4
Jesus10011 <torralbagonzalezjesusfrancisco@gmail.com>
2026-03-13 20:02:03 +0100

    Segundo commit

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        mostrarFecha();
+    }
+
+    public static void mostrarFecha() {
+        LocalDateTime date = LocalDateTime.now();
+        System.out.println(date);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tarea de Entornos
-Autor: **Jesús Fco. Torralba González**
-Año: **2026**
+Autor: **Jesús Fco. Torralba González**  
+Año: **2026**  
+
+## Funcionalidad
+  
+El programa ahora cuenta con un método que permite visualizar la fecha actual.
